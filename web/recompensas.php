b7252cd53e5c7a9099219705a0f6260cec52d917
f3rxx0-18 <frndrtr@gmail.com>
2024-06-07 20:44:06 -0600

    cambioosss

---
+++ web/recompensas.php
@@ -0,0 +1,74 @@
+!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="preload" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preload" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>CecyHaQ</title>
+</head>
+
+<body>
+    <header class="header__nosotros">
+        <a href="">
+            <img class="nosotros__img" src="img/cecyhacklogo.png" alt="">
+        </a>
+        <nav class="nosotros__nav">
+            <a href="#">Nosotros</a>
+            <a href="#">Sistema de recompensas</a>
+            <a href="#">Patrocinadores</a>
+            <a href="">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EFEFEF">
+                    <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
+                </svg>
+
+            </a>
+        </nav>
+    </header>
+    <main class="informacion__nosotros">
+        <div class="nostros__explicacion">
+            <h1 class="titulo centrado">¡Toma el control de tu consumo eléctrico y ahorra dinero!</h1>
+
+            <p>¿Te gustaría saber cuánto gastas realmente en electricidad?</p>
+
+            <h2 class="subtitulo">Te ofrecemos soluciones a medida para reducir tu consumo y ahorrar dinero:</h2>
+
+            <h3>Sin inversión:</h3>
+
+            <ul>
+                <li><span class="bold">Consejos personalizados</span>: Recibirás recomendaciones para optimizar el uso de tus electrodomésticos y hábitos de consumo.</li>
+                <li><span class="bold">Monitorización inteligente</span>: Te ayudamos a identificar los dispositivos que más energía consumen y cómo reducir su impacto.</li>
+                <li><span class="bold">Programa de cashback</span>: Obtén descuentos en tu próximo recibo de luz por adoptar prácticas sostenibles.</li>
+            </ul>
+
+            <h3>Con inversión en energía renovable:</h3>
+
+            <ul>
+                <li>Asesoría para la instalación de paneles solares: Genera tu propia energía limpia y reduce drásticamente tu dependencia de la red eléctrica.</li>
+                <li>Financiamiento flexible: Te acompañamos en todo el proceso, desde la evaluación de tu potencial solar hasta la obtención de financiamiento accesible.</li>
+                <li>Monitoreo continuo de tu sistema solar: Garantizamos el óptimo funcionamiento de tu sistema y maximizamos tu ahorro.</li>
+            </ul>
+
+            <p>Moderniza tu hogar y disfruta de un futuro más verde y económico.</p>
+            <p>¡Toma el control de tu energía y tu futuro!</p>
+
+        </div>
+    </main>
+    <footer class="centrado">
+        @ CECYHAQ 2024 - All rights reserved
+    </footer>
+
+
+</body>
+
+</html>
