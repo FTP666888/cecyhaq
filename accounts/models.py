0ac3349d78ca7e3e6162aa50a3b2f112344e8c3f
CrxxpCherry <fakecarlosflores1998@gmail.com>
2024-06-07 18:47:24 -0600

    changin to php

--- accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=128)
-
