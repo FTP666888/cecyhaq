241f93db31b113fd66bb4d54c18bfa68a5d6fcc4
CrxxpCherry <fakecarlosflores1998@gmail.com>
2024-06-07 19:42:09 -0600

    adding info and stuff

--- index.php
+++ index.php
@@ -3,12 +3,61 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preload" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>login</title>
 </head>
 
 <body>
-    
+    <header class="header__nosotros">
+        <a href="">
+            <img class="index__img" src="" alt="">
+        </a>
+        <nav class="nosotros__nav">
+            <a href="#">Nosotros</a>
+            <a href="#">Sistema de recompensas</a>
+            <a href="#">Patrocinadores</a>
+        </nav>
+    </header>
+    <main class="informacion__nosotros">
+        <div class="nostros__explicacion">
+            <h1>¡Toma el control de tu consumo eléctrico y ahorra dinero!</h1>
+
+            <p>¿Te gustaría saber cuánto gastas realmente en electricidad?</p>
+
+            <h2>Te ofrecemos soluciones a medida para reducir tu consumo y ahorrar dinero:</h2>
+
+            <h3>Sin inversión:</h3>
+
+            <ul>
+                <li>Consejos personalizados: Recibirás recomendaciones para optimizar el uso de tus electrodomésticos y hábitos de consumo.</li>
+                <li>Monitorización inteligente: Te ayudamos a identificar los dispositivos que más energía consumen y cómo reducir su impacto.</li>
+                <li>Programa de cashback: Obtén descuentos en tu próximo recibo de luz por adoptar prácticas sostenibles.</li>
+            </ul>
+
+            <h3>Con inversión en energía renovable:</h3>
+
+            <ul>
+                <li>Asesoría para la instalación de paneles solares: Genera tu propia energía limpia y reduce drásticamente tu dependencia de la red eléctrica.</li>
+                <li>Financiamiento flexible: Te acompañamos en todo el proceso, desde la evaluación de tu potencial solar hasta la obtención de financiamiento accesible.</li>
+                <li>Monitoreo continuo de tu sistema solar: Garantizamos el óptimo funcionamiento de tu sistema y maximizamos tu ahorro.</li>
+            </ul>
+
+            <p>Moderniza tu hogar y disfruta de un futuro más verde y económico.</p>
+            <p>¡Toma el control de tu energía y tu futuro!</p>
+
+        </div>
+    </main>
+    <footer>
+        @ CECYHACK - All rights reserved
+    </footer>
+
 
 </body>
 
